086c58111e932c673a00459386554335aa0d53d4
Phi <phihdn@gmail.com>
2025-06-06 16:57:07 -0300

    add languages

--- main_internal_test.go
+++ main_internal_test.go
@@ -8,10 +8,36 @@ func Example() {
 	// Hello world
 }
 
-func TestGreet(t *testing.T) {
+func TestGreet_English(t *testing.T) {
+	lang := language("en")
 	want := "Hello world"
 
-	got := greet()
+	got := greet(lang)
+
+	if got != want {
+		// mark this test as failed
+		t.Errorf("expected: %q, got: %q", want, got)
+	}
+}
+
+func TestGreet_French(t *testing.T) {
+	lang := language("fr")
+	want := "Bonjour le monde"
+
+	got := greet(lang)
+
+	if got != want {
+		// mark this test as failed
+		t.Errorf("expected: %q, got: %q", want, got)
+	}
+}
+
+func TestGreet_Akkadian(t *testing.T) {
+	// Akkadian is not implemented yet!
+	lang := language("akk")
+	want := ""
+
+	got := greet(lang)
 
 	if got != want {
 		// mark this test as failed
