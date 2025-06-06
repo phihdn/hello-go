e90ece917d0c2f1368e962d430508df178f4e916
Phi <phihdn@gmail.com>
2025-06-06 16:54:32 -0300

    greet() function

--- main_internal_test.go
+++ main_internal_test.go
@@ -1,7 +1,20 @@
 package main
 
+import "testing"
+
 func Example() {
 	main()
 	// Output:
 	// Hello world
+}
+
+func TestGreet(t *testing.T) {
+	want := "Hello world"
+
+	got := greet()
+
+	if got != want {
+		// mark this test as failed
+		t.Errorf("expected: %q, got: %q", want, got)
+	}
 }
